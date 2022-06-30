fd08dcaec9254bed7ecc81bf856f4d42d36624e5
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-06-30 04:13:00 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
+/**
+ * main - Entry point
  *
- *main function to print out something
+ * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
