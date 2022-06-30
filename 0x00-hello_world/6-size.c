fbd0cd97721664e8e318abf3de24c595ef622d1f
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-06-30 12:55:22 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 
 /**
  * main - printsout the size of some variables and their type
- * Return - if ok, returns 0, else something else
+ * return - if ok, returns 0, else something else
  */
 int main(void)
 {
-printf(stderr, "size of a char: %d byte(s)\n", sizeof(char));
-printf(stderr, "size of an int: %d byte(s)\n", sizeof(int));
-printf(stderr, "size of a long int: %d byte(s)\n", sizeof(long int));
-printf(stderr, "size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf(stderr, "size of a float: %d byte(s)\n", sizeof(float));
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
