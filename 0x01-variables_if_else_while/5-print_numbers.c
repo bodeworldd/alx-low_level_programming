cdb5c941f433b1c292724b2864945fba592b2273
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-01 20:50:29 +0100

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - assign random number to variable
+*
+* Return: 0 (Sucessful)
+*/
+int main(void)
+{
+int n;
+for (n=0; n<10; n++)
+{
+	printf("%d\n", n);
+}
+return (0);
+}
