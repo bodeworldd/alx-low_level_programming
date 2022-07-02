40dd1fb4ad56fa40b7ecc113eb70eabbfa8f563e
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-02 21:34:56 +0100

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints comma-separated digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 48; i <= 57; i++)
+        {
+			for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+		{     
+				
+				putchar(i);
+                putchar(j);
+				if (i != 56 || j != 57)
+                {
+                        putchar(44);
+                        putchar(32);
+                }
+                    putchar(10);
+					return (0);
+		}
+		}
+		}
+}
