83718aa74e98109b82f3da8ca5aa8b2a43d0cc82
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-02 21:49:57 +0100

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,23 @@
  */
 int main(void)
 {
-        int i;
-	int j;
-        for (i = 48; i <= 57; i++)
-        {
-			for (j = 49; j <= 57; j++)
-		{
-			if (j > i)
-		{     
-				
-				putchar(i);
-                putchar(j);
-				if (i != 56 || j != 57)
-                {
-                        putchar(44);
-                        putchar(32);
-                }
-                    putchar(10);
-		}
-		}
-		}
-		return (0);
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (j > i)
+{     
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+return (0);
 }
