b30986868a2b4ff7377f1eebed37939abd12d1e7
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-05 13:38:25 +0100

    to check 1-10

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* _islower - 1 if c is a letter, lowercase or uppercase, 0 otherwise
-* @c: _islower argument
+* _isalpha - 1 if c is a letter, lowercase or uppercase, 0 otherwise
+* @c: _isalpha argument
 * Return: 1 if c is lowercase, 0 otherwise
 */
 
