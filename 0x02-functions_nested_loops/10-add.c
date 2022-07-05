0132b02ee9f6e18c17172ea1c9697843acd89c6b
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-05 14:15:29 +0100

    mandatory conclusion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - sums two numbers
+*@n:first inte
+*@m:second int
+*Return: returns 0
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,13 @@
 int _putchar(char c);
-void print_alphabet(void);
+vvoid print_alphabet(void);
+void jack_bauer(void);
 void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int add(int n, int m);
+int print_last_digit(int n);
