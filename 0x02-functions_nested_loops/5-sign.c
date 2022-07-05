e5fad6cbb2add5edbf497dd1684634bf293310ce
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-05 13:39:46 +0100

    to check 1-10

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
 * print_sign - heck the code.
 * @n: print_sign argument
-* Return: Returns 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero,-1 and prints - if n is less than zero
+* Return: Returns 1 and prints + if n is greater than zero,
 */
 
 int print_sign(int n)
