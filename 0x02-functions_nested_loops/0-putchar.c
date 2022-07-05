0f50fc16833beba3a5ff80c3954218e767662ad0
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-05 13:10:32 +0100

    to check 1-10

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
 * main - print _putchar along with a space.
 */
 
-int main (void)
+int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
