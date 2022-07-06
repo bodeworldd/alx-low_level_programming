19b49092eac0f2d6ad49923093e63f9f7b4d7b20
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-06 09:41:34 +0100

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
-* main - results to an infinite loop
-* Return: 0
-*/
+ * main - main
+ * Return: 0
+ */
 
 int main(void)
 {
 int i;
-
-printf("Infinite loop incoming :(\n");
-
 i = 0;
-/*
-*while (i < 10)
-*{
-*putchar(i);
-*}
-*/
-printf("Infinite loop avoided! \\o/\n");
+
+positive_or_negative(i);
 
 return (0);
+
 }
