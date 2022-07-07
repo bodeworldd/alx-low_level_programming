374abc717f63f06143499a7948a70b2720cf9a68
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-07 17:50:23 +0100

    mandatory

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,10 @@
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -10,6 +13,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+
+#endif /* MAIN_H */
