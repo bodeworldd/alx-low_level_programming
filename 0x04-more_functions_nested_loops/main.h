6378bbc1aab544ed993681066b94848b171f9655
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-08 14:35:10 +0100

    mandatory

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -10,3 +15,4 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+#endif
