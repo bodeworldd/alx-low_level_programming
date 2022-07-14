c8fe780496cb626edd1edac47cf8b3d621880c80
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-14 14:27:30 +0100

    mandatory

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,32 @@
 #include "main.h"
 /**
-*cap_string - capitalize first character of a word
-*@str: string
+*cap_string - function that capitalize first character
+*@str: string to capitalize
 *Return:returns the capitalized string
 */
 char *cap_string(char *str)
 {
-int index = 0;
+	int index = 0;
 
-while (str[++index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}')
-str[index] -= 32;
-}
-return (str);
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
-* leet - encode a string
-*@str:string
+* leet - function that encode strings
+*@str:string that will be encoded
 *Return:returns encoded string
 */
+
 char *leet(char *str)
 {
-int index1 = 0, index2;
-char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-while (str[++index1])
-{
-for (index2 = 0; index2 <= 7; index2++)
-{
-if (str[index1] == leet[index2] ||
-str[index1] - 32 == leet[index2])
-str[index1] = index2 + '0';
-}
-}
-return (str);
+	while (str[++index1])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+			 str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
+		}
+	}
+	return (str);
 }
