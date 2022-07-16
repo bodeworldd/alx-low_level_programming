7900e00e32dd7576f77d8e2768b1d2d074466592
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-07-16 07:38:43 +0100

    completions

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - Prints int
+ * @n: An integer
+ */
+void print_number(int n)
+{
+	unsigned int i, x, flag;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -n;
+	}
+	else
+	{
+		i = n;
+	}
+	x = i;
+	flag = 1;
+	while (i >= 10)
+	{
+		i /= 10;
+		flag *= 10;
+	}
+	for (; flag >= 1; flag /= 10)
+	{
+		_putchar(((x / flag) % 10) + 48);
+	}
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,45 @@
 #include "main.h"
+
 /**
-*cap_string - function that capitalize first character
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
-char *cap_string(char *str)
+ * cap_string - Changes all words to title case
+ * @c: Array of characters
+ *
+ * Return: Titled String
+ */
+char *cap_string(char *c)
 {
-	int index = 0;
+	int i, flag;
+	char *ptr, delimiters[] = {' ', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	while (str[++index])
+	ptr = c;
+	if (*c >= 97 && *c <= 122)
+		*c -= 32;
+	c++;
+	flag = 0;
+	while (*c)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}')
-str[index] -= 32;
+		if ((*c > 47 && *c < 58) || (*c > 64 && *c < 91))
+			flag = 0;
+		else if (*c >= 97 && *c <= 122 && flag)
+		{
+			flag = 0;
+			*c -= 32;
+		}
+		else if (*c < 32)
+			flag = 1;
+		else
+		{
+			for (i = 0; i < 11; i++)
+			{
+				if (delimiters[i] == *c)
+				{
+					flag = 1;
+				}
+			}
+		}
+		c++;
 	}
-	return (str);
+	return (ptr);
 }
+
