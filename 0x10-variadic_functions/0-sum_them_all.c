2430fbcfb8b8947bba53652792b8f398dd875400
Balogun Saheed <saheed.olabode.balogun@gmail.com>
2022-08-05 09:20:05 +0100

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,9 +4,7 @@
 
 /**
  * sum_them_all - A function that sums all parameters
- * va_start - va_start.
  * @n: Parameter count
- * @aa: aa
  * @...: Other parameter
  * Return: Sum of all parameters
  */
